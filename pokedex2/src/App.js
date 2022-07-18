f3a811362788fc237a117b357a7c5403dd8458eb
Nicole Prim <nicolepprim@gmail.com>
2022-07-18 15:10:56 -0300

    header, router, coordinator

--- pokedex2/src/App.js
+++ pokedex2/src/App.js
@@ -1,9 +1,15 @@
-
+import Header from "./Components/Header/Header";
+import {Router} from '../src/Router/Router'
+import Cards from "./Components/Cards/Cards";
+import DetailsPoke from "./Pages/DetailsPoke/DetailsPoke";
 
 function App() {
   return (
     <div>
-     
+      <Router />
+      <Cards />
+<DetailsPoke />
+     Pokedex
     </div>
   );
 }
--- pokedex2/src/Components/Cards/Cards.js
+++ pokedex2/src/Components/Cards/Cards.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
+import { goToDetailsPoke } from '../../Router/Coordinator'
 
 const Cards = () => {
+  const navigate = useNavigate()
   return (
-    <div>Cards</div>
+    <div>
+      Cards
+      <button onClick={() => goToDetailsPoke(navigate)}>Ver detalhes</button>
+    </div>
   )
 }
 
