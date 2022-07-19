23a2f345a8d284d598fe5e17dba5313ae96b014e
Nicole Prim <nicolepprim@gmail.com>
2022-07-19 15:52:05 -0300

    estilização pagina detalhes

--- pokedex2/src/Pages/DetailsPoke/DetailsPoke.js
+++ pokedex2/src/Pages/DetailsPoke/DetailsPoke.js
@@ -1,11 +1,28 @@
 import React from 'react'
-import Header from '../../Components/Header/Header'
+import { BigContainer, DetailsContainer, Titulo, CardUm, CardDois, CardTres, CardQuatro } from './DetailsStyled'
 
 const DetailsPoke = () => {
   return (
-    <div>
-      DetailsPoke
-      </div>
+    <BigContainer>
+      <Titulo>
+        <h1>DetailsPoke</h1>
+      </Titulo>
+      <DetailsContainer>
+        <p>oi</p>
+        <CardUm>
+
+        </CardUm>
+        <CardDois>
+
+        </CardDois>
+        <CardTres>
+
+        </CardTres>
+        <CardQuatro>
+
+        </CardQuatro>
+      </DetailsContainer>
+    </BigContainer>
   )
 }
 
---
+++ pokedex2/src/Pages/DetailsPoke/DetailsStyled.js
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+
+export const BigContainer = styled.section`
+background-color: #5e5e5e;
+position: relative;
+width: 100%;
+height: 74rem;
+`
+export const Titulo = styled.div`
+h1{
+width: 13.75rem;
+height: 4.5rem;
+padding-left: 2.5rem;
+padding-top: 13.75rem;
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 3rem;
+line-height: 4.5rem;
+color: #FFFFFF;
+}
+`
+
+export const DetailsContainer = styled.div`
+background-color: #729f92;
+margin-left: 1.6rem;
+margin-right: 1.6rem;
+position: absolute;
+width: 87rem;
+height: 42rem;
+border-radius: 2.368rem;
+
+`
+
+export const CardUm = styled.div`
+position: absolute;
+width: 282px;
+height: 282px;
+left: 44px;
+top: 26px;
+background: url(image.png), #FFFFFF;
+border: 2px solid #FFFFFF;
+border-radius: 8px;
+`
+
+export const CardDois = styled.div`
+position: absolute;
+width: 282px;
+height: 282px;
+left: 44px;
+top: 355px;
+background: #FFFFFF;
+border: 2px solid #FFFFFF;
+border-radius: 8px;
+`
+export const CardTres = styled.div`
+position: absolute;
+width: 343px;
+height: 613px;
+left: 360px;
+top: 24px;
+
+background: #FFFFFF;
+border-radius: 12px;
+`
+export const CardQuatro = styled.div`
+position: absolute;
+width: 292px;
+height: 453px;
+left: 771px;
+top: 184px;
+
+background: #FFFFFF;
+border-radius: 8px;
+`
