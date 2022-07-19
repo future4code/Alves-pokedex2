fde2c9e416cf9a647c9a8beebdf7905283fd4ebc
Rafael santos de sousa <rafael.santos259@hotmail.com>
2022-07-19 15:52:31 -0300

    styled botão excluir

---
+++ pokedex2/src/Pages/Pokedex/PokedexStyled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+ export const PokedexStyled = styled.h3`
+
+`
+export const Botao= styled.button`
+position:absolute;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 4px 10px;
+
+width: 146px;
+height: 38px;
+
+background: #FF6262;
+border-radius: 8px;
+color: white;
+`
+
+
+    
