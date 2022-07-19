898c31094150dc67bfccdf8d148885169f9ad1f8
Ilena Acioli dos Santos <ilenaaciolids@gmail.com>
2022-07-19 18:39:08 -0300

    estilização card

--- pokedex2/src/Components/Cards/CardsStyled.js
+++ pokedex2/src/Components/Cards/CardsStyled.js
@@ -1,47 +1,37 @@
 import styled from "styled-components";
 
 export const ContainerCard = styled.div`
-width: 440px;
-height: 210px;
-left: 0px;
-top: 0px;
+width: 25rem;
+height: 13.125rem;
 background: #729F92;
-border-radius: 12px;
-
+border-radius:0.75rem;
 `
-
 export const Id = styled.div`
 position: absolute;
-width: 30px;
-height: 19px;
-padding-left: 23px;
-padding-top: 25px;
-
+width: 1.875rem;
+height: 1.188rem;
+padding-left:1.438rem;
+padding-top: 1.563rem;
 font-family: 'Inter';
 font-style: normal;
 font-weight: 700;
-font-size: 16px;
-line-height: 19px;
-
+font-size: 1rem;
+line-height: 1.188rem;
 color: #FFFFFF;
-border: 1px solid black;
 `
 
 export const Name = styled.div`
 position: absolute;
-width: 159px;
-height: 39px;
-padding-left: 23px;
-padding-top: 40px;
-
+width: 9.938rem;
+height: 2.438rem;
+padding-left: 1.438rem;
+padding-top: 2.5rem;
 font-family: 'Inter';
 font-style: normal;
 font-weight: 700;
-font-size: 32px;
-line-height: 39px;
-
+font-size: 2rem;
+line-height: 2.438rem;
 color: #FFFFFF;
-border: 1px solid black;
 `
 
 export const Type = styled.div`
@@ -49,40 +39,68 @@ position: absolute;
 display: flex;
 flex-direction: row;
 align-items: flex-start;
-padding: 5px 8px;
-gap: 17px;
-
-position: absolute;
-width: 99px;
-height: 31px;
-left: 23px;
-top: 89px;
-
+gap: 1.063rem;
+width: 6.188rem;
+height: 1.938rem;
+margin-left: 1.438rem;
+margin-top: 5.563rem;
 background: #AD61AE;
 border: 1px dashed rgba(255, 255, 255, 0.47);
-border-radius: 8px;
-
+border-radius: 0.5rem;
 `
 
 export const TextType = styled.div`
-width: 46px;
-height: 21px;
-
+width: 2.875rem;
+height: 1.313rem;
 font-family: 'Poppins';
 font-style: normal;
 font-weight: 400;
-font-size: 14px;
-line-height: 21px;
-/* identical to box height */
-
-
+font-size:0.875rem;
+line-height: 1.313rem;
+padding-top: 0.313rem;
+padding-bottom: 0.375rem;
+padding-right: 0.5rem;
 color: #FFFFFF;
-
-
-/* Inside auto layout */
-
 flex: none;
 order: 1;
 flex-grow: 0;
+`
 
+export const Icon = styled.img`
+padding-top: 0.313rem;
+padding-left:0.5rem;
+padding-bottom: 0.375rem;
+`
+
+export const TextDetails = styled.div`
+position: absolute;
+width: 4.625rem;
+height: 1.5rem;
+padding-top:10.375rem ;
+padding-left:1.438rem;
+padding-bottom: 1.25rem ;
+padding-right: 21.438rem;
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 1rem;
+line-height:1.5rem;
+text-decoration-line: underline;
+color: #FFFFFF;
+flex: none;
+order: 0;
+flex-grow: 0;
+`
+
+export const Button = styled.button`
+position: absolute;
+width: 9.125rem;
+height: 2.375rem;
+background: #FFFFFF;
+border-radius: 0.5rem;
+border: none;
+margin-top: 9.938rem ;
+margin-left: 15rem ;
+margin-bottom: 0.813rem ;
+margin-right: 1.375rem;
 `
