cce67afd3fbe4f564e7d3a74720d39851c701a21
Nicole Prim <nicolepprim@gmail.com>
2022-07-19 17:51:00 -0300

    estilização header

---
+++ pokedex2/src/Components/Header/HeaderStyled.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+export const ContainerHeader = styled.div`
+display: flex;
+width: 100%;
+height: 10rem;;
+align-items: center;
+justify-content: center;
+background: #FFFFFF;
+`
+
+export const ButtonPokedex = styled.button`
+
+font-family: 'Poppins', sans-serif;
+background-color: #33A4F5;
+font-style: normal;
+font-weight: 700;
+color: #FFFFFF;
+font-size: 24px;
+line-height: 36px;
+border-radius: 8px;
+border: none;
+width: 17.938rem;
+height: 4.625rem;
+cursor: pointer;
+margin-left: 80%;
+`
+
+export const ButtonHome = styled.button `
+color: #1A1A1A;
+cursor: pointer;
+border: none;
+height: 36px;
+left: 100px;
+position: absolute;
+width: 210px;
+height: 36px;
+left: 100px;
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 36px;
+text-decoration-line: underline;
+color: #1A1A1A;
+
+`
+
+
+export const DivImg = styled.div`
+display: flex;
+position: absolute;
+justify-content: center;
+width: 307px;
+height: 113px;
+img {
+width: 19.188rem;
+height: 7.06rem;
+}
+`
